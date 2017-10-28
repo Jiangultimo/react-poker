7f9e7a9487dba949c536995965c42f38ef352545
Neras <medianeras57@gmail.com>
2017-10-28 22:37:01 +0800

    add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'normalize.css';
 import Navs from './components/common/navs/navs.js';
 class DemoList extends React.Component{
     constructor(props){
