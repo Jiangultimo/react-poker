c5213e44e9d26b116130f5a3d9cfb2a3ba6462df
Neras <medianeras57@gmail.com>
2017-10-28 23:12:40 +0800

    complete navs

--- src/components/common/navs/navs.js
+++ src/components/common/navs/navs.js
@@ -23,15 +23,22 @@ class Navs extends React.Component{
             ]
         };
     }
-
+    handleActive(e){//点击导航只有一个被选中
+        let links = document.getElementsByClassName('navs-a__link');//类数组
+        let len = links.length;
+        for(let i = 0; i<len; i++){
+            links[i].style.backgroundColor = '#fff';
+        }
+        e.target.style.backgroundColor='#e3e3e3';
+    }
     render(){
         return(
             <div className="navs-div__container">
             <Router>
-                <ul>
+                <ul className="navs-ul__group">
                     {
                         this.state.navs.map( (value, index) => {
-                            return <li key={index}><Link to={value.link}>{value.name}</Link></li>
+                            return <li key={index} onClick={this.handleActive} className="navs-li__item"><Link to={value.link} className="navs-a__link">{value.name}</Link></li>
                         })
                     }
                 </ul>
