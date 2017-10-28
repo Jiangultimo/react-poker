29a329e407d133282039e8f6459f7018aeeda8be
Neras <medianeras57@gmail.com>
2017-10-28 22:25:42 +0800

    init index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Navs from './components/common/navs/navs.js';
+class DemoList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <Navs />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <DemoList />,
+    document.getElementById('app')
+);
