dc4db905156b5da515fedfda2feb7b7b708418b1
Neras <medianeras57@gmail.com>
2017-11-13 23:16:45 +0800

    add inline style

--- src/components/Editor/editor.js
+++ src/components/Editor/editor.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOm from 'react-dom';
 import Immutable from 'immutable';
 
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 import './editor.css';
 
-console.log(Immutable);
 const blockRenderMap = Immutable.Map({
     'header-two':{
         element:'h2'
@@ -15,7 +14,7 @@ const blockRenderMap = Immutable.Map({
         element:'h2'
     }
 });
-
+const INLINE_STYLE = ['BOLD','ITALIC'];
 class MEditor extends React.Component{
     constructor(props){
         super(props);
@@ -24,28 +23,32 @@ class MEditor extends React.Component{
         };
         this.onChange = this.onChange.bind(this);
         this.editorFocus = this.editorFocus.bind(this);
-        this.getContent = this.getContent.bind(this);
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
     }
     
+    _onChangeStyle(item){
+        let newState = RichUtils.toggleInlineStyle(this.state.editorState, item);
+        this.onChange(newState);
+    }
+    handleKeyCommand(command){
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        if(newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
     componentDidMount(){
         this.refs['editorWrapper'].addEventListener('click',this.editorFocus);
-        // this.refs['output'].addEventListener('click', this.getContent);
     }
     onChange(content){
         this.setState({
             editorState:content
         });
     }
-    getContent(){
-        console.log(this.state.editorState.getCurrentContent().hasText());
-    }
     editorFocus(){//点击wrapper的时候，编辑器也获取焦点
         this.refs['editor'].focus();
         this.refs['placeholder'].style.display = 'none';
-        // this.refs['editor'].blur(function(){
-        //     console.log('blur');
-        //     this.refs['placeholder'].style.display = 'block';
-        // })
     }
     /** 
      * 接下来是编辑器部分，基于draftjs https://draftjs.org/
@@ -53,12 +56,19 @@ class MEditor extends React.Component{
     render(){
         return (
             <div className="editor-div__wrapper" ref="editorWrapper">
+            <div className="editor-div__tool">
+            {
+                INLINE_STYLE.map( (item, index) => {
+                    return <button className="editor-button__item" data-style={item} key={index} onClick={this._onChangeStyle.bind(this, item)}>{item}</button>
+                })
+            }
+            </div>
                 <span className="editor-span__placeholder" ref="placeholder">从这里你可以开始输入...</span>
                 <Editor 
                     className="editor-div__panel" 
+                    handleKeyCommand={this.handleKeyCommand}
                     editorState={this.state.editorState}
                     onChange={this.onChange} 
-                    blockRenderMap={blockRenderMap}
                     ref="editor"
                 />
             </div>
